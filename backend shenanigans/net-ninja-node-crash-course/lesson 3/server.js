d3d8b304d6ed0a732aafdf181351a8cbdf0718b4
Debayo Tomisin <debayotomisin07@gmail.com>
2022-11-03 19:39:39 +0100

    lesson-4/net-ninja node js crash course

--- backend shenanigans/net-ninja-node-crash-course/lesson 3/server.js
+++ backend shenanigans/net-ninja-node-crash-course/lesson 3/server.js
@@ -1,7 +1,41 @@
-const http = require("http"); // this is used to create a server.
+const http = require("http");
+const fs = require("fs"); // this is used to create a server.
 
 const server = http.createServer((req, res) => {
-  console.log("request made!");
+  res.setHeader("Content-Type", "text/html");
+
+  //using the switch statement to figure out what url the user visited.
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (error, data) => {
+    if (error) {
+      console.log(error);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
